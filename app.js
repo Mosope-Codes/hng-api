753e5bfa9c135c233c7cbe5dfc69a644776aeada
Mosope <moshopeowo@gmail.com>
2022-10-28 08:59:43 +0100

    'changed end-point'

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const { application } = require("express");
 var express = require("express");
 var app = express();
 
-app.get("/user-detail", (req, res) => {
+app.get("/", (req, res) => {
     res.json({ "slackUsername": "Mosope", "backend": true, "age": 21, "bio": "I'm a student of Unilag studying Mathematics" });
 });
 
