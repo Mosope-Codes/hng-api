4d3b1da2f617539653dc7f339841b269936c83e9
Mosope <moshopeowo@gmail.com>
2022-10-28 08:24:56 +0100

    init

---
+++ app.js
@@ -0,0 +1,12 @@
+const { application } = require("express");
+var express = require("express");
+var app = express();
+
+app.get("/user-detail", (req, res) => {
+    res.json({ "slackUsername": "Mosope", "backend": true, "age": 21, "bio": "I'm a student of Unilag studying Mathematics" });
+});
+
+let port = 3000;
+app.listen(port, () => {
+    console.log(`The app is listening at port ${port}...`)
+})
